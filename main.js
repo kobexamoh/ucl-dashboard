e1f6aad52d0cfbcb124f9e80453e46dfeae509df
Kobe Amoh <kobexamoh@gmail.com>
2024-09-27 23:10:50 -0600

    Add TODOs for tomorrow's work

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ const userAPIKey = import.meta.env.VITE_API_KEY;
 console.log(userAPIKey);
 console.log("working!");
 console.log(userAPIKey);
+
+// TODO: Check if package.json needs to be added to git structure (as it's getting quite huge)
+// TODO: move application files into src folder
+// TODO: probably delete public folder
