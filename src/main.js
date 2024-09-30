298bd881edc333a65a1202f6d1307af5036e1da4
Kobe Amoh <kobexamoh@gmail.com>
2024-09-29 23:43:47 -0600

    Fetch leagues from Football API to confirm all is working

--- src/main.js
+++ src/main.js
@@ -3,7 +3,20 @@ const userAPIKey = import.meta.env.VITE_API_KEY;
 
 // test if API key is exporting correctly
 console.log(userAPIKey);
-console.log("working!");
-console.log(userAPIKey);
 
-// TODO: move application files into src folder
+
+// make a test API call to serve Real Madrid UCL information
+let myHeaders = new Headers();
+
+myHeaders.append("x-rapidapi-key", userAPIKey);
+myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+
+let requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow"
+};
+
+fetch("https://v3.football.api-sports.io/leagues", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
